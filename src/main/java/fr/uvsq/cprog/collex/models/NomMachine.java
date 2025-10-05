0176b3ea791a6ab9036a1fac56c604ba1a942a96
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2025-10-05 12:05:52 +0200

    feat: add NomMachine class and its unit tests

---
+++ src/main/java/fr/uvsq/cprog/collex/models/NomMachine.java
@@ -0,0 +1,34 @@
+package fr.uvsq.cprog.collex.models;
+
+public class NomMachine {
+
+    private final String value;
+
+    public NomMachine(String value) {
+        // Regex pour valider un nom de machine (ex: www.uvsq.fr)
+        String regex = "^(?!-)([A-Za-z0-9-]+\\.)+[A-Za-z]{2,}$";
+
+        if (value == null || !value.matches(regex)) {
+            throw new IllegalArgumentException("Nom de machine invalide : " + value);
+        }
+
+        this.value = value;
+    }
+
+    public String getMachine() {
+        return value.split("\\.")[0];
+    }
+
+    public String getDomaine() {
+        return value.substring(value.indexOf('.') + 1);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
