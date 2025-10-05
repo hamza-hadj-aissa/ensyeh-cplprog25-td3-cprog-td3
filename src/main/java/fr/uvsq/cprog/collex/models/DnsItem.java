d4997868120817c0c912f9eded228e521ec1cf62
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2025-10-05 12:20:22 +0200

    feat: add DnsItem class and its unit tests

---
+++ src/main/java/fr/uvsq/cprog/collex/models/DnsItem.java
@@ -0,0 +1,24 @@
+package fr.uvsq.cprog.collex.models;
+
+public class DnsItem {
+    private final AdresseIP ip;
+    private final NomMachine nom;
+
+    public DnsItem(AdresseIP ip, NomMachine nom) {
+        this.ip = ip;
+        this.nom = nom;
+    }
+
+    public AdresseIP getIp() {
+        return ip;
+    }
+
+    public NomMachine getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return ip + " " + nom;
+    }
+}
