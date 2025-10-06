3d2580ca9c63801cd554af61ea24f88bf2c0ebad
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2025-10-06 22:03:40 +0200

    docs: added documentation for DnsItem

--- src/main/java/fr/uvsq/cprog/collex/models/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/models/DnsItem.java
@@ -1,9 +1,18 @@
 package fr.uvsq.cprog.collex.models;
 
+/**
+ * Represents a DNS item containing an IP address and a machine name.
+ */
 public class DnsItem {
   private final AdresseIp ip;
   private final NomMachine nom;
 
+  /**
+   * Constructs a DnsItem with the specified IP address and machine name.
+   *
+   * @param ip  the IP address
+   * @param nom the machine name
+   */
   public DnsItem(AdresseIp ip, NomMachine nom) {
     this.ip = ip;
     this.nom = nom;
