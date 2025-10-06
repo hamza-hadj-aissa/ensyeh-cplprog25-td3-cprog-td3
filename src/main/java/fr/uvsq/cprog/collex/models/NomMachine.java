e65550936e75a29b9a5793cc3af9588c80bda274
Hamza HADJ AISSA <hadjaissahamza@gmail.com>
2025-10-06 21:57:03 +0200

    docs: added documentation for NomMachine

--- src/main/java/fr/uvsq/cprog/collex/models/NomMachine.java
+++ src/main/java/fr/uvsq/cprog/collex/models/NomMachine.java
@@ -1,9 +1,22 @@
 package fr.uvsq.cprog.collex.models;
 
+/**
+ * Represents a machine name (hostname) with validation.
+ * A machine name consists of a name and a domain, for example: www.uvsq.fr
+ */
 public class NomMachine {
 
   private final String value;
 
+  /**
+   * Constructs a NomMachine object with the specified value.
+   *
+   * <p>Expects a valid FQDN (Fully Qualified Domain Name) format.
+   * Ex: sciences.uvsq.fr, mail.google.com</p>
+   *
+   * @param value the machine name to validate and store
+   * @throws IllegalArgumentException if the value is null or does not match the required pattern
+   */
   public NomMachine(String value) {
     // Regex pour valider un nom de machine (ex: www.uvsq.fr)
     String regex = "^(?!-)([A-Za-z0-9-]+\\.)+[A-Za-z]{2,}$";
@@ -14,6 +27,7 @@ public class NomMachine {
 
     this.value = value;
   }
+
 
   public String getMachine() {
     return value.split("\\.")[0];
